49414143d72da7264083e86f8387ad18cbb62557
“lezix” <78815730@qq.com>
2024-11-10 20:43:39 +0800

    build feishu yao

--- docker/build/build.sh
+++ docker/build/build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 cd /app && \
-git clone https://github.com/yaoapp/kun.git /app/kun && \
-git clone https://github.com/yaoapp/xun.git /app/xun && \
-git clone https://github.com/yaoapp/gou.git /app/gou && \
-git clone https://github.com/yaoapp/v8go.git /app/v8go && \
-git clone https://github.com/yaoapp/xgen.git /app/xgen-v1.0 && \
-git clone https://github.com/yaoapp/yao-init.git /app/yao-init && \
-git clone https://github.com/yaoapp/yao.git /app/yao
+git clone https://github.com/feishu/kun.git /app/kun && \
+git clone https://github.com/feishu/xun.git /app/xun && \
+git clone https://github.com/feishu/gou.git /app/gou && \
+git clone https://github.com/feishu/v8go.git /app/v8go && \
+git clone https://github.com/feishu/xgen.git /app/xgen-v1.0 && \
+git clone https://github.com/feishu/yao-init.git /app/yao-init && \
+git clone https://github.com/feishu/yao.git /app/yao
 
 
 cd /app/yao && \
